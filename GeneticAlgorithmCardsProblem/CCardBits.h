b0fbaafd5036645840b95a462747930d5e9bedd2
Kretek <szelinski.p@gmail.com>
2018-04-27 20:20:57 +0200

    initial commit

--- GeneticAlgorithmCardsProblem/CCardBits.h
+++ GeneticAlgorithmCardsProblem/CCardBits.h
@@ -12,12 +12,16 @@ public:
 
 	void vCrossOver(CCardBits & c_first, CCardBits & c_other, int i_cross_posi);
 
+	double dGetDistance();
+
 	int iTotalSum() const;
 
 	int iTotalProduct() const;
 
 private:
 	int i_size;
+	double d_distance;
+	bool b_was_changed;
 	vector<bool> v_bits;
 }; // class CCardBits
 
